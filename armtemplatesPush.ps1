90e2ee4e58b943f0278da23b268d275c561f2206
Steven Messenger [DATACOM] <StevenMe@datacom.co.nz>
2017-09-20 18:17:58 +1200

    test

---
+++ armtemplatesPush.ps1
@@ -0,0 +1,10 @@
+﻿cd "C:\Users\stevenme\Documents\GitHub\armtemplates"
+git add -A
+Write-Host "`n$(git status | Out-String)" -ForegroundColor Cyan
+if ((git status | Out-String) -ilike "*Changes to be committed*"){
+    $message = Read-Host "Enter a commit message"
+    Write-Host "`n$(git commit -m $message -v | Out-String)" -ForegroundColor Yellow
+    Write-Host "`n$(git push -v | Out-String)" -ForegroundColor Cyan
+    Write-Host "`nDone..." -ForegroundColor Green
+}
+Read-Host "Press Enter to exit" | Out-String
